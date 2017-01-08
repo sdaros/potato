8e3ce5927cd49c13d78dc3977b63d53fb70fa65a
Stefano Da Ros <sdaros@cip.li>
2017-01-08 14:20:15 +0100

    Simplify potato and make it use libnotify

--- potato.sh
+++ potato.sh
@@ -2,77 +2,42 @@
 
 WORK=25
 PAUSE=5
-INTERACTIVE=true
-MUTE=false
 
 show_help() {
-	cat <<-END
-		usage: potato [-s] [-m] [-w m] [-b m] [-h]
-		    -s: simple output. Intended for use in scripts
-		        When enabled, potato outputs one line for each minute, and doesn't print the bell character
-		        (ascii 007)
-
-		    -m: mute -- don't play sounds when work/break is over
-		    -w m: let work periods last m minutes (default is 25)
-		    -b m: let break periods last m minutes (default is 5)
-		    -h: print this message
-	END
+  cat <<-END
+    usage: potato [-w m] [-b m] [-h]
+    -w m: let work periods last m minutes (default is 25)
+    -b m: let break periods last m minutes (default is 5)
+    -h: print this message
+END
 }
 
-while getopts :sw:b:m opt; do
-	case "$opt" in
-	s)
-		INTERACTIVE=false
-	;;
-	m)
-		MUTE=true
-	;;
-	w)
-		WORK=$OPTARG
-	;;
-	b)
-		PAUSE=$OPTARG
-	;;
-	h|\?)
-		show_help
-		exit 1
-	;;
-	esac
+while getopts :w:b:m opt; do
+  case "$opt" in
+    w)
+      WORK=$OPTARG
+      ;;
+    b)
+      PAUSE=$OPTARG
+      ;;
+    h|\?)
+      show_help
+      exit 1
+      ;;
+  esac
 done
 
-time_left="%im left of %s "
-
-if $INTERACTIVE; then
-	time_left="\r$time_left"
-else
-	time_left="$time_left\n"
-fi
+puts() {
+  notify_summary="$1"
+  notify_body="$2"
+  notify-send -t 0 -u normal "$1" "$2"
+}
 
 while true
 do
-	for ((i=$WORK; i>0; i--))
-	do
-		printf "$time_left" $i "work"
-		sleep 1m
-	done
+  puts "Pomodoro" "Get Started"
+  sleep "$WORK"m
 
-	! $MUTE && aplay /usr/share/sounds/speech-dispatcher/test.wav &>/dev/null
-
-	if $INTERACTIVE; then
-		echo -e "\a"
-		echo "Work over"
-		read
-	fi
-
-	for ((i=$PAUSE; i>0; i--))
-	do
-		printf "$time_left" $i "pause"
-		sleep 1m
-	done
-	! $MUTE && aplay /usr/share/sounds/speech-dispatcher/test.wav &>/dev/null
-	if $INTERACTIVE; then
-		echo -e "\a"
-		echo "Pause over"
-		read
-	fi
+  puts "Pomodoro" "Take a break"
+  sleep "$PAUSE"m
 done
